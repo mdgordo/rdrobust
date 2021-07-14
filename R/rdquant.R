001c9cd2a509cb3af47306f1380b81b054002299
mdgordo <mdgordo@mdgordos-MBP.home>
2021-07-14 13:33:03 -0400

    added quantile treatment effects

--- R/rdquant.R
+++ R/rdquant.R
@@ -5,29 +5,31 @@
 #' @param x a numeric vector
 #' @param fuzzy a binary vector indicating dummy for treatment
 #' @param c a double for the cutoff point
-#' @return a data frame of y values, coefficients, and standard errors
+#' @param grid a double between 0 and 1 controlling how finely to estimate the pdfs
+#' @param qstep a double between 0 and 1 controlling which quantile treatment effects to return
+#' @return list of coefficients, standard errors and quantile treatment effect standard errors
 #' @export
 
-rdquant <- function(Y, x, fuzzy = NULL, c = 0, ...){
-        yvals <- quantile(Y, seq(.05, .95, .05))
-        out_1 <- data.frame("yvals" = yvals,
-                            "coef" = rep(NA, length(yvals)),
-                            "se" = rep(NA, length(yvals)),
-                            "treatment" = rep(1, length(yvals)))
-        out_0 <- out_1
-        out_0$treatment <- 0
+rdquant <- function(data, x, fuzzy = NULL, c = 0, grid = .01, qstep = .05, indices, ...){
         if (is.null(fuzzy)) fuzzy <- ifelse(Y>c, 1, 0)
         fuzzy0 <- 1 - fuzzy
-        for (i in c(1:length(yvals))) {
-                yi <- as.numeric(Y<yvals[i])
-                y1d <- yi*fuzzy
-                y0d <- yi*fuzzy0
-                rd1out <- rdrobust(y = y1d, x = x, fuzzy = fuzzy, c = c, ...)
-                rd0out <- rdrobust(y = y0d, x = x, fuzzy = fuzzy0, c = c, ...)
-                out_1$coef[i] <- rd1out$coef["Conventional",]
-                out_1$se[i] <- rd1out$se["Robust",]
-                out_0$coef[i] <- rd0out$coef["Conventional",]
-                out_0$se[i] <- rd0out$se["Robust",]
+        yvals1 <- quantile(Y[fuzzy==1], seq(.05, .95, grid))
+        yvals0 <- quantile(Y[fuzzy0==1], seq(.05, .95, grid))
+        coefs <- function(qinv, f){
+                y1d <- as.numeric(Y<qinv)*f
+                rd1out <- rdrobust(y = y1d, x = x, fuzzy = f, c = c, ...)
+                return(rd1out)
         }
-        return(rbind(out_1, out_0))
+        mods1 <- lapply(yvals1, FUN = coefs, f = fuzzy)
+        mods0 <- lapply(yvals0, FUN = coefs, f = fuzzy0)
+        coefs1 <- lapply(mods1, function(x) x$coef["Conventional",])
+        coefs0 <- lapply(mods0, function(x) x$coef["Conventional",])
+        ses1 <- lapply(mods1, function(x) x$se["Robust",])
+        ses0 <- lapply(mods0, function(x) x$se["Robust",])
+        r1 <- rearrangement(x = data.frame(yvals1), y = coef1)
+        r0 <- rearrangement(x = data.frame(yvals0), y = coef0)
+        q1 <- sapply(seq(.05, .95, qstep), function(x) min(yvals1[r1>x]))
+        q0 <- sapply(seq(.05, .95, qstep), function(x) min(yvals0[r0>x]))
+        q <- q1 - q0
+        return(list(coefs1, coefs0, ses1, ses0, q))
 }
