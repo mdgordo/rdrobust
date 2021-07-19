b62c93e0e83bdb066f7c34dc4400f1dee132d0ca
mdgordo <mdgordo@gmail.com>
2021-07-19 15:37:40 -0400

    Untreated estimate

--- R/rdgazer.R
+++ R/rdgazer.R
@@ -8,19 +8,19 @@
 #' @export
 
 rdgazer <- function(rdlist, dvlabs = NULL, xlines = NULL, ...){
-        dummymods <- list(); coef <- list(); se <- list(); bw <- c(); nobs <- c(); depvarmean <- c()
+        dummymods <- list(); coef <- list(); se <- list(); bw <- c(); nobs <- c(); untreatedmean <- c()
         for (i in 1:length(rdlist)) {
                 dummymods[[i]] <- lm(rdlist[[i]]$Y ~ rdlist[[i]]$X)
                 coef[[i]] <- c(0, rdlist[[i]]$coef["Conventional",])
                 se[[i]] <- c(1, rdlist[[i]]$se["Robust",])
                 bw[i] <- round(rdlist[[i]]$bws[1,1],2)
                 nobs[i] <- sum(rdlist[[i]]$N_h)
-                depvarmean[i] <- round(weighted.mean(rdlist[[i]]$Y, rdlist[[i]]$W, na.rm = TRUE),2)
+                untreatedmean[i] <- round(rdlist[[i]]$beta_p_l,2)
         }
         s <- stargazer(dummymods, type = "text", coef = coef, se = se, column.labels = dvlabs,
                        omit.stat = "all", digits = 2, df = FALSE, omit = c("Constant"), 
                        covariate.labels = c("Treatment"),
                        add.lines = c(list(c("N", nobs),
                                           c("Bandwidth", bw),
-                                          c("Dep Var Mean", depvarmean)), xlines), ...)
+                                          c("Untreated Estimate", untreatedmean)), xlines), ...)
 }
